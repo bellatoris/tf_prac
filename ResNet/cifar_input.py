f0887077a96868d4ece9ac471d184d9f1e716706
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-09 19:09:19 +0000

    Adding tarfile member sanitization to extractall()

--- ResNet/cifar_input.py
+++ ResNet/cifar_input.py
@@ -28,7 +28,26 @@ def build_input(data_path, batch_size, mode):
     local_file = base.maybe_download(CIFAR10, data_path, SOURCE_URL + CIFAR10)
     if not os.path.exists(ROOT):
         with tarfile.open(local_file, "r:gz") as tar:
-            tar.extractall(data_path)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, data_path)
             tar.close()
 
     image_size = 32
